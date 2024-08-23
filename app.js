350e01d2b4bc5d492c70e039fbc6528004d5916a
Youssef AbuTaleb <youssefabdelmoneim134@gmail.com>
2024-08-24 00:09:18 +0300

    adding routes and conrollers

--- app.js
+++ app.js
@@ -1,69 +1,29 @@
-const fs = require('fs');
 const express = require('express');
+const morgan = require('morgan');
+
+const tourRouter = require('./routes/tourRoutes');
+const userRouter = require('./routes/userRoutes');
 
 const app = express();
 
-//Middleware
+// 1) MIDLLEWARES
 app.use(express.json());
-
-// app.get('/', (req, res) => {
-//   res
-//     .status(200)
-//     .json({ message: 'Hello from the server side', app: 'Natours' });
-// });
-
-// app.post('/', (req, res) => {
-//   res.send('You can post to this endpoint....');
-// });
-
-const tours = JSON.parse(
-  fs.readFileSync(`${__dirname}/dev-data/data/tours-simple.json`)
-);
-
-app.get('/api/v1/tours', (req, res) => {
-  res.status(200).json({
-    status: 'success',
-    results: tours.length,
-    data: { tours },
-  });
+app.use((req, res, next) => {
+  console.log('Hello from the middleware');
+  next();
 });
-
-app.get('/api/v1/tours/:id', (req, res) => {
-  const id = +req.params.id;
-
-  const tour = tours.find((el) => el.id === id);
-
-  if (!tour) {
-    return res.status(404).json({ status: 'failed', message: 'Invalid ID' });
-  }
-
-  res.status(200).json({
-    status: 'success',
-    data: { tour },
-  });
+app.use((req, res, next) => {
+  req.requestTime = new Date().toISOString();
+  next();
 });
+app.use(morgan('dev'));
 
-app.post('/api/v1/tours', (req, res) => {
-  const newId = tours[tours.length - 1].id + 1;
-  const newTour = Object.assign({ id: newId }, req.body);
+// 3) ROUTES
 
-  tours.push(newTour);
-
-  fs.writeFile(
-    `${__dirname}/dev-data/data/tours-simple.json`,
-    JSON.stringify(tours),
-    (err) => {
-      res.status(201).json({
-        status: 'success',
-        data: {
-          tour: newTour,
-        },
-      });
-    }
-  );
-});
-
+app.use('/api/v1/tours', tourRouter);
+app.use('/api/v1/users', userRouter);
+// 4) START SERVER
 const port = 3000;
 app.listen(port, () => {
-  console.log(`App running on port ${port}`);
+  console.log(`Running on port ${port}`);
 });
