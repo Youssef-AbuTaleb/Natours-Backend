1c444b8d61f0cf42d1bb9624e959399b7464756d
Youssef AbuTaleb <youssefabdelmoneim134@gmail.com>
2024-09-16 22:34:48 +0300

    handle wrong routes

--- app.js
+++ app.js
@@ -10,10 +10,7 @@ const app = express();
 // 1) MIDLLEWARES
 app.use(express.json());
 app.use(express.static(`${__dirname}/public`));
-app.use((req, res, next) => {
-  console.log('Hello from the middleware');
-  next();
-});
+
 app.use((req, res, next) => {
   req.requestTime = new Date().toISOString();
   next();
@@ -27,5 +24,11 @@ if (process.env.NODE_ENV === 'development') {
 
 app.use('/api/v1/tours', tourRouter);
 app.use('/api/v1/users', userRouter);
+app.all('*', (req, res) => {
+  res.status(404).json({
+    status: 'fail',
+    message: `Can't find ${req.originalUrl} on this server!`,
+  });
+});
 
 module.exports = app;
